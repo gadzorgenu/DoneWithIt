f3e9597d4c2d5bca080bc4ef0273bd12b21ce723
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-02-03 23:48:37 +0000

    Initail commit

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView,StatusBar, Platform, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      {/* <Text>Hello world!!</Text> */}
+      <Button
+        color='orange'
+        title='Click Me'
+      //   onPress={()=> Alert.alert('My title', 'My message',
+      //   [{ text: 'Yes', onPress: ()=> console.log('Yes')},
+      //   {text:'No', onPress: ()=>console.log('No')}])
+      // }
+      onPress={()=>
+        Alert.prompt('My title','My message',text=>console.log(text))
+      }
+      />
+      {/* <Image 
+        source={require('./assets/icon.png')}
+      /> */}
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
