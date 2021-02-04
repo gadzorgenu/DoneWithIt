f2757f67a8f2a73c6d49ee6e69e14ad651ddd024
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-02-04 23:33:01 +0000

    Modified viewImageScreen

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
 
+import colors from '../config/colors'
+
 function ViewImageScreen(props) {
     return (
         <View
@@ -21,20 +23,20 @@ const styles = StyleSheet.create({
     close:{
         width: 50,
         height: 50,
-        backgroundColor:'#fc5c65',
+        backgroundColor:colors.primary,
         position: 'absolute', // to position the component relative to its parent 
         top: 40,
         left:30
 
     },
     container:{
-        backgroundColor:'#000',
+        backgroundColor:colors.black,
         flex:1 //this makes the view take or cover the entire screen
     },
     delete:{
         width: 50,
         height: 50,
-        backgroundColor:'#4ecdc4',
+        backgroundColor:colors.secondary,
         position: 'absolute', // to position the component relative to its parent 
         top: 40,
         right:30
