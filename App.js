af9ccdd3ba38206e0f5a4df140cab580a7da308d
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-02-05 23:23:41 +0000

    Created the AppText Component

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { View } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
-
+import {MaterialCommunityIcons} from '@expo/vector-icons'
+import AppText from './app/components/AppText'
 export default function App() {
   
   return (
-    // <WelcomeScreen/>
-    <ViewImageScreen/>
+   <View
+      style={{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center'
+      }}
+   >
+      <MaterialCommunityIcons 
+        name='email' 
+        size={60} 
+        color='dodgerblue'
+      />
+   </View>
   );
 }
 
---
+++ app/components/AppText.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text, StyleSheet, Platform } from 'react-native'
+
+function AppText({ children}) {
+    return (
+        <Text style={styles.text} >{children}</Text>
+    );
+}
+
+
+
+const styles = StyleSheet.create({
+    text:{
+        color:'tomato',
+        //spreading the platfrom object to copy its properties into the text object
+        ...Platform.select({
+            ios:{
+                 fonSize: 20,
+                 fontFamily:'Avenir'
+            },
+            android: {
+                fonSize: 18,
+                fontFamily: 'Roboto'
+            }
+        })
+    }
+})
+
+export default AppText;
