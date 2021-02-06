a0854909dc7eb8e3646f71d0eaa8d51a316c2452
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-02-06 01:24:27 +0000

    Modified the view image screen

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default function App() {
     //     image={require('./app/assets/jacket.jpg')}
     //   />
     // </View>
-    <ListingDetailsScreen/>
+    <ViewImageScreen/>
   );
 }
 
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
-
+import { MaterialCommunityIcons} from '@expo/vector-icons'
 import colors from '../config/colors'
 
 function ViewImageScreen(props) {
@@ -8,8 +8,20 @@ function ViewImageScreen(props) {
         <View
             style={styles.container}
         >
-            <View style={styles.close}></View>
-            <View style={styles.delete}></View>
+            <View style={styles.close}>
+                <MaterialCommunityIcons 
+                    name='close' 
+                    color='white' 
+                    size={30}
+                />
+            </View>
+            <View style={styles.delete}>
+                <MaterialCommunityIcons
+                    name='trash-can-outline'
+                    color='white'
+                    size={30}
+                />
+            </View>
             <Image 
             resizeMode='contain'
                 style={styles.image}
@@ -21,22 +33,15 @@ function ViewImageScreen(props) {
 
 const styles = StyleSheet.create({
     close:{
-        width: 50,
-        height: 50,
-        backgroundColor:colors.primary,
         position: 'absolute', // to position the component relative to its parent 
         top: 40,
         left:30
-
     },
     container:{
         backgroundColor:colors.black,
         flex:1 //this makes the view take or cover the entire screen
     },
     delete:{
-        width: 50,
-        height: 50,
-        backgroundColor:colors.secondary,
         position: 'absolute', // to position the component relative to its parent 
         top: 40,
         right:30
