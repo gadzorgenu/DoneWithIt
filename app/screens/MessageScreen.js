d3b0c23180f189bb4f183a20249c80a5900485b6
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-02-08 00:42:31 +0000

    No change

--- app/screens/MessageScreen.js
+++ app/screens/MessageScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FlatList,StyleSheet, View } from 'react-native';
 import ListItem from '../components/ListItem';
-import ListItemDeleteAction from '../components/ListItemDeleteAction';
 import ListItemSeparator from '../components/ListItemSeparator';
 import Screen from '../components/Screen';
 
@@ -25,14 +24,21 @@ const MessageScreen = () => {
         <FlatList
           data={ messages}
           keyExtractor={ message=> message.id.toString()}
-           renderItem={({item}) => (
+          renderItem={({item}) => (
               <ListItem 
               title={item.title}
               subTitle={item.description}
               image={item.image}
               onPress={()=> console.log('Message selected', item)}
-              // renderRightActions is a function for rendering basic view
-              renderRightActions={()=> <ListItemDeleteAction onPress={()=>console.log('Message Selected', item) }/>}
+              renderRightActions={()=> 
+                <View
+                    style={{
+                        backgroundColor: 'red',
+                        width: 70
+                    }}
+              >
+              </View>
+              }
           />
           )}
           ItemSeparatorComponent={ListItemSeparator }
