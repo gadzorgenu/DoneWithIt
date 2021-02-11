3991013dd54217fab704e626ea8363901ffcd874
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-02-11 22:41:50 +0000

    Added Acount detail component

--- App.js
+++ App.js
@@ -8,22 +8,21 @@ import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessageScreen from './app/screens/MessageScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App() {
   
   return (
-    // <View style={{
-    //   backgroundColor:'#f8f4f4',
-    //   padding: 20,
-    //   paddingTop: 100
-    // }}>
-    //   <Card
-    //     title='Red jacket for sale'
-    //     subTitle='$100'
-    //     image={require('./app/assets/jacket.jpg')}
-    //   />
-    // </View>
-    <MessageScreen/>
+    // <MessageScreen/>
+    <Screen>
+       <Icon
+          name="email"
+          size={50}
+          backgroundColor='red'
+          iconColor='white'
+       />
+    </Screen>
   );
 }
 
