f0e4cf71d4d96666f6fad86e87a9aa52aa1e5425
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-02-11 23:44:10 +0000

    Added listing screen

--- App.js
+++ App.js
@@ -12,12 +12,12 @@ import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
 
 export default function App() {
   
   return (
-    // <MessageScreen/>
-    <AccountScreen/>
+ <ListingsScreen/>
   );
 }
 
--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Constants from 'expo-constants'
-import {SafeAreaView, StyleSheet} from 'react-native'
+import {SafeAreaView, StyleSheet,View} from 'react-native'
 
 const Screen = ({ children, style}) => {
     return (
        <SafeAreaView style={[styles.screen, style]}>
-           {children}
+           <View style={style}>
+                {children}
+           </View>
        </SafeAreaView>
     );
 }
