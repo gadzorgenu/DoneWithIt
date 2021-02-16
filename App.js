6a0caf36a2769a6a220685e4c3d84c99a0854e75
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-02-16 23:50:25 +0000

    Modified add

--- App.js
+++ App.js
@@ -1,22 +1,10 @@
 import React,{useState} from 'react';
-import { Switch, Text, TextInput, View } from 'react-native';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import {MaterialCommunityIcons} from '@expo/vector-icons'
-import AppText from './app/components/AppText'
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessageScreen from './app/screens/MessageScreen';
 import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker'
 import LoginScreen from './app/screens/LoginScreen';
+
 export default function App() {
+
   const [firstName, setFirstName] = useState('')
   const [isNew, setIsNew] = useState(false)
   const [category, setCategory] = useState()
