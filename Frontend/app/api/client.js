b2829d6f284699b3a35228ff69f7103e6c6dd9da
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-02-23 23:17:43 +0000

    Added backend

---
+++ Frontend/app/api/client.js
@@ -0,0 +1,31 @@
+import { create } from 'apisauce'
+
+//the apisauce library is used for calling apis. 
+//It is a wrapper around axios, which shows standardized errors and transforms request/response objects easily
+//Whenever you call a server, the underlining promise is already resolved so you don not have to add an extra trycatch block 
+//just take the response to see if there is an error 
+
+const apiClient = create({
+    //url of your backend server. localhost doesn't work
+    baseURL: 'http://192.168.8.126:9000/api'
+})
+
+// apiCreate.get('/listings').then(response => {
+//     if(!response.ok){
+//         response.problem
+//     }
+// }) 
+
+export default apiClient
+
+
+
+
+
+
+
+
+
+
+
+9
