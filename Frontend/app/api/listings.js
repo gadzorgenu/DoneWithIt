19740f142911718883134a41de7460028c800014
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-02-26 00:16:02 +0000

    Added post method

--- Frontend/app/api/listings.js
+++ Frontend/app/api/listings.js
@@ -1,8 +1,31 @@
-// import client from './client'
+// // import client from './client'
 
-// const endpoint = '/listings'
-// const getListings = () => client.get(endpoint)
+// const { add } = require("react-native-reanimated")
 
-// export default {
-//     getListings
+// // const endpoint = '/listings'
+// // const getListings = () => client.get(endpoint)
+
+// const addListing = listing => {
+//     const data = new FormData()
+//     data.append('title',listing.title)
+//     data.append('price',listing.price)
+//     data.append('categoryId',listing.category.value)
+//     data.append('description',listing.description)
+
+//     listing.images.forEach( image => 
+//         data.append('images', {
+//             name: 'image' + index,
+//             type: 'image/jpeg',
+//             uri: image
+//         }))
+
+//     if(listing.location)
+//     data.append('location', JSON.stringify(listing.location))
+
+//    return client.post(endpoint, data )
 // }
+
+// // export default {
+// //     getListings,
+// //     addListing
+// // }
