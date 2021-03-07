e483c4b8b32db25052d3a5433b9fc146a7ad0d96
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-03-07 22:48:23 +0000

    Modified app

---
+++ Frontend/app/context/listings.js
@@ -0,0 +1,36 @@
+import React, { createContext} from 'react'
+import Axios from 'axios'
+import PropTypes from 'prop-types'
+import axios from 'axios'
+
+export const ListingsContext = createContext()
+
+const ListingsContextProvider = ({children}) => {
+    
+    const addListing = async ( listing) => {
+        try {
+            await axios({
+                method:'POST',
+                url:'http://192.168.8.126:9000/listings'
+            })
+        } catch (error) {
+            return error 
+        }
+    }
+
+    return(
+        <ListingsContext.Provider
+            value={{
+                addListing
+            }}
+        >
+            {children}
+        </ListingsContext.Provider>
+    )
+}
+
+ListingsContextProvider.propTypes = {
+    children: PropTypes.node.isRequired
+}
+
+export default ListingsContextProvider
